9550d38fca912765d2352c1af7977e42bc45a3cb
Aditya <adi.shirsat3004@gmail.com>
2019-01-14 14:44:15 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AdminComponent } from './admin/admin.component';
+import { ScorecardComponent } from './scorecard/scorecard.component';
+import { NewmatchComponent } from './admin/newmatch/newmatch.component';
+import { SelectteamComponent } from './admin/selectteam/selectteam.component';
+import { SummaryComponent } from './summary/summary.component';
+import { CommentryComponent } from './commentry/commentry.component';
+import {NgPipesModule} from 'ngx-pipes';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    AdminComponent,
+    ScorecardComponent,
+    NewmatchComponent,
+    SelectteamComponent,
+    SummaryComponent,
+    CommentryComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgPipesModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
